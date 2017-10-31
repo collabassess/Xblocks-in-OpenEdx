a70df296837c1b680ba4555d3bde17b69d7a6c22
Ajay Thorve <AjayThorve@users.noreply.github.com>
2017-10-31 10:58:46 -0400

    Update setup.py

--- togetherjsxblock/setup.py
+++ togetherjsxblock/setup.py
@@ -23,7 +23,7 @@ def package_data(pkg, roots):
 
 setup(
     name='togetherjsxblock-xblock',
-    version='0.1',
+    version='0.2',
     description='togetherjsxblock XBlock',   # TODO: write a better description.
     license='UNKNOWN',          # TODO: choose a license: 'AGPL v3' and 'Apache 2.0' are popular.
     packages=[
