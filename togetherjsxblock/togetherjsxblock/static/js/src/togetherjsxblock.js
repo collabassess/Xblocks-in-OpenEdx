a7293f52932605e94f664642b212258645a859b5
Ajay Thorve <ajaythorve1994@gmail.com>
2017-11-26 18:17:04 -0500

    Update, user name on chatbox

--- togetherjsxblock/togetherjsxblock/static/js/src/togetherjsxblock.js
+++ togetherjsxblock/togetherjsxblock/static/js/src/togetherjsxblock.js
@@ -1,18 +1,18 @@
 /* Javascript for TogetherJsXBlock. */
-function TogetherJsXBlock(runtime, element) {
+function TogetherJsXBlock(runtime, element, data) {
 
     function updateRoom(result) {
         $('.room', element).text(result.room);
         TogetherJSConfig_findRoom = {prefix:result.room, max: 2};
-
+        alert("room added:"+result.room)
     }
 
     function updateUserName(result) {
         TogetherJSConfig_getUserName = result.s_name;
         alert(result.s_name+" it works");
-//        TogetherJS.config("suppressJoinConfirmation", function () {
-//          return true;
-//        });
+        TogetherJS.config("suppressJoinConfirmation", function () {
+          return true;
+        });
     }
 
 
@@ -22,6 +22,8 @@ function TogetherJsXBlock(runtime, element) {
 
 
     $(function ($) {
+//        updateVotes(data)
+
         TogetherJS.config("disableWebRTC", function () {
               return true;
             });
@@ -31,6 +33,15 @@ function TogetherJsXBlock(runtime, element) {
         TogetherJS.config("suppressJoinConfirmation", function () {
           return true;
         });
+
+        TogetherJS.config("cloneClicks", function () {
+          return true;
+        });
+
+        TogetherJS.config("includeHashInUrl", function () {
+          return true;
+        });
+
         TogetherJSConfig_hubBase = "https://calm-escarpment-25279.herokuapp.com/";
 
         TogetherJS.config("dontShowClicks",function(){
@@ -47,12 +58,44 @@ function TogetherJsXBlock(runtime, element) {
             success: updateRoom
         });
 
+        $.ajax({
+            type: "POST",
+            url: handlerUrl,
+            data: JSON.stringify({"hello": "world1"}),
+            success: updateUserName
+        });
+
+
+
+    });
+
+
+
+//
+//    function updateVotes(votes) {
+//        $('.upvote .count', element).text(votes.up);
+//        $('.downvote .count', element).text(votes.down);
+//    }
+//
+//    var handlerUrl = runtime.handlerUrl(element, 'vote');
+//
+//    $('.upvote', element).click(function(eventObject) {
 //        $.ajax({
 //            type: "POST",
 //            url: handlerUrl,
-//            data: JSON.stringify({"hello": "world1"}),
-//            success: updateUserName
+//            data: JSON.stringify({voteType: 'up'}),
+//            success: updateVotes
 //        });
+//    });
+//
+//    $('.downvote', element).click(function(eventObject) {
+//        $.ajax({
+//            type: "POST",
+//            url: handlerUrl,
+//            data: JSON.stringify({voteType: 'down'}),
+//            success: updateVotes
+//        });
+//    });
 
-    });
+
 }
