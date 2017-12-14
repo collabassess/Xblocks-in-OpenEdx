c870ebe9accaa87579bf4dbb81bd582127c1192d
Ajay Thorve <ajaythorve1994@gmail.com>
2017-12-14 02:14:02 -0500

    Added new block

---
+++ shareable_content_xblock/shareable_content_xblock/shareable_content_xblock.py
@@ -0,0 +1,190 @@
+"""TO-DO: Write a description of what this XBlock is."""
+
+import pkg_resources
+import logging
+
+from xblock.core import XBlock
+from xblock.fields import Integer, Scope,String, DateTime, Boolean
+from xblock.fragment import Fragment
+from xblockutils.studio_editable import StudioEditableXBlockMixin
+
+log = logging.getLogger(__name__)
+
+logging.basicConfig(level = logging.ERROR)
+
+logging.disable(logging.CRITICAL)
+logging.disable(logging.DEBUG)
+logging.disable(logging.INFO)
+import MySQLdb
+import settings as s
+
+@XBlock.needs("i18n")
+@XBlock.wants('user')
+class ShareContentXBlock(XBlock):
+    """
+    TO-DO: document what your XBlock does.
+    """
+
+    # Fields are defined on the class.  You can access them in your code as
+    # self.<fieldname>.
+
+    # TO-DO: delete count, and define your own fields.
+    count = Integer(
+        default=0, scope=Scope.user_state,
+        help="A simple counter, to show something happening",
+    )
+    room = String(
+        default="room", scope=Scope.settings,
+        help="A chat room number",
+    )
+    s_name = String(default="a", scope=Scope.settings, help="user name")
+
+    editable_fields = ('s_name', 'room')
+
+    def resource_string(self, path):
+        """Handy helper for getting resources from our kit."""
+        data = pkg_resources.resource_string(__name__, path)
+        return data.decode("utf8")
+
+    # TO-DO: change this view to display your data your own way.
+    def student_view(self, context=None):
+        """
+        The primary view of the ShareContentXBlock, shown to students
+        when viewing courses.
+        """
+        html = self.resource_string("static/html/shareable_content_xblock.html")
+        frag = Fragment(html.format(self=self))
+        frag.add_css(self.resource_string("static/css/shareable_content_xblock.css"))
+        frag.add_javascript(self.resource_string("static/js/src/shareable_content_xblock.js"))
+        frag.add_javascript(self.resource_string("static/js/jquery.js"))
+        frag.initialize_js('ShareContentXBlock')
+        return frag
+
+    # TO-DO: change this handler to perform your own actions.  You may need more
+    # than one handler, or you may not need any handlers at all.
+    @XBlock.json_handler
+    def get_room(self,data,suffix=''):
+        cnx = MySQLdb.connect(**s.database)
+        cursor = cnx.cursor()
+        curr_user = self.get_userid()
+        cursor.execute("""
+                    SELECT group_id,course_id from user_groups
+                    WHERE user1=%s OR user2=%s
+                    """,(curr_user,curr_user))
+        for (group_id,course_id) in cursor:
+            log.error("ye")
+            self.room = str(group_id+"-"+course_id)
+            return {"room": self.room}
+
+    @XBlock.json_handler
+    def submit_ans(self, data, suffix=''):
+        log.error(data['user1'])
+        sol = data['user1']
+        cnx = MySQLdb.connect(**s.database)
+        cursor = cnx.cursor()
+        curr_user = self.get_userid()
+        cursor.execute("""SELECT * FROM user_hint_solutions where user_id=%s""",curr_user)
+        if not cursor.rowcount:
+            log.error("New row created"+","+curr_user+","+sol)
+            cursor.execute("""
+                               INSERT INTO user_hint_solutions
+                                VALUES(%s,%s,%s)
+                           """, ('1',sol,str(curr_user)))
+            cnx.commit()
+        else:
+            log.error("update")
+            for (Question_id, ans, user_id) in cursor:
+                log.error(Question_id+","+user_id+","+sol)
+                log.error('1,'+curr_user)
+                cursor.execute("""
+                                UPDATE user_hint_solutions
+                                SET ans=%s
+                                WHERE Question_id=%s AND user_id=%s
+                                """, (sol,'1',curr_user))
+
+                cnx.commit()
+                break
+        return "done"
+
+    @XBlock.json_handler
+    def get_ans_self(self,data,suffix=''):
+        cnx = MySQLdb.connect(**s.database)
+        cursor = cnx.cursor()
+        curr_user = self.get_userid()
+        cursor.execute("""SELECT ans FROM user_hint_solutions where user_id=%s""", (curr_user))
+        if not cursor.rowcount:
+            return " "
+        else:
+            log.error("else")
+            for ans in cursor:
+                log.error(ans)
+                return ans
+        return "failed"
+
+
+    @XBlock.json_handler
+    def get_ans_ptnr(self,data,suffix=''):
+        cnx = MySQLdb.connect(**s.database)
+        cursor = cnx.cursor()
+        curr_user = self.get_userid()
+        cursor.execute("""SELECT user1,user2 FROM user_groups where user1=%s OR user2=%s""", (curr_user,curr_user))
+        if not cursor.rowcount:
+            return "user has no partner"
+        else:
+            log.error("else")
+            for (user1,user2) in cursor:
+                log.error(user1+","+user2+":"+curr_user)
+                if user1 == curr_user:
+                    partner = user2
+                else:
+                    partner = user1
+                log.error("partner:"+partner)
+                cursor.execute("""
+                                SELECT ans FROM user_hint_solutions where user_id=%s AND Question_id='1'
+                                """,(partner))
+                if not cursor.rowcount:
+                    return "partner has not answered yet"
+                else:
+                    for ans in cursor:
+                        log.error(ans)
+                        return ans
+        return "failed"
+
+    @XBlock.json_handler
+    def returnUserName(self, data, suffix=''):
+        """
+           a handler which returns user name.
+        """
+        return {"s_name": self.get_user().full_name}
+
+    def get_user(self):
+        """Get an attribute of the current user."""
+        user_service = self.runtime.service(self, 'user')
+        if user_service:
+            # May be None when creating bok choy test fixtures
+            return user_service.get_current_user()
+        return None
+
+    def get_userid(self):
+        try:
+            return self.get_user().opt_attrs['edx-platform.user_id']
+        except:
+            return '4'
+
+    # TO-DO: change this to create the scenarios you'd like to see in the
+    # workbench while developing your XBlock.
+    @staticmethod
+    def workbench_scenarios():
+        """A canned scenario for display in the workbench."""
+        return [
+            ("ShareContentXBlock",
+             """<shareable_content_xblock/>
+             """),
+            ("Multiple ShareContentXBlock",
+             """<vertical_demo>
+                <shareable_content_xblock/>
+                <shareable_content_xblock/>
+                <shareable_content_xblock/>
+                </vertical_demo>
+             """),
+        ]
---
+++ shareable_content_xblock/shareable_content_xblock/static/js/src/shareable_content_xblock.js
@@ -0,0 +1,64 @@
+/* Javascript for ShareContentXBlock. */
+function ShareContentXBlock(runtime, element) {
+
+
+    $("#stu_sol").click(function(){
+        var dataString = $('#stu_form').serializeArray().reduce(function(obj, item) {
+                        obj[item.name] = item.value;
+                        return obj;
+                    }, {});
+        $(".solution").text("hint is: "+dataString.user1);
+        $.ajax({
+            type: "POST",
+            url: runtime.handlerUrl(element, 'submit_ans'),
+            data: JSON.stringify(dataString),
+            success: function(result){
+                console.log(result);
+            }
+        });
+
+
+
+    });
+
+    function get_ans_ptnr(){
+        $.ajax({
+            type: "POST",
+            url: runtime.handlerUrl(element, 'get_ans_ptnr'),
+            data:JSON.stringify({"hello": "world"}),
+            success: function(result){
+                SelectRadioButton("user2",result[0]);
+                $(".solution2").text("hint is: "+result[0]);
+            }
+        });
+    }
+
+    function check_ans_self(){
+        $.ajax({
+            type: "POST",
+            url: runtime.handlerUrl(element, 'get_ans_self'),
+            data:JSON.stringify({"hello": "world"}),
+            success: function(result){
+                SelectRadioButton("user1",result[0]);
+                $(".solution").text("hint is: "+result[0]);
+            }
+        });
+    }
+
+    function SelectRadioButton(name, value) {
+
+      $("input[name='"+name+"'][value='"+value+"']").prop('checked', true);
+
+      return false; // Returning false would not submit the form
+
+    }
+
+    $(function ($) {
+
+         check_ans_self();
+
+         get_ans_ptnr();
+
+         setInterval(get_ans_ptnr, 1000);
+    });
+}
