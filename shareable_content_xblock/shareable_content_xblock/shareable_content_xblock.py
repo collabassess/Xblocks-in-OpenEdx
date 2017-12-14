8a5b9cd3d9749704c83f6874ca6efc59687a677a
Ajay Thorve <ajaythorve1994@gmail.com>
2017-12-14 02:31:07 -0500

    Jquery cdn try

--- shareable_content_xblock/shareable_content_xblock/shareable_content_xblock.py
+++ shareable_content_xblock/shareable_content_xblock/shareable_content_xblock.py
@@ -56,7 +56,6 @@ class ShareContentXBlock(XBlock):
         frag = Fragment(html.format(self=self))
         frag.add_css(self.resource_string("static/css/shareable_content_xblock.css"))
         frag.add_javascript(self.resource_string("static/js/src/shareable_content_xblock.js"))
-        frag.add_javascript(self.resource_string("static/js/jquery.js"))
         frag.initialize_js('ShareContentXBlock')
         return frag
 
